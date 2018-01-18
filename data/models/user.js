47b24f6cea870595bf87a7159e51af70cc32ce9f
robki <robin@ruddervoorde.be>
2018-01-18 12:13:21 +0100

    controle done

--- data/models/user.js
+++ data/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+var request = require("request");
 let userSchema = require("../schemas/user");
 let User = mongoose.model("User",userSchema,"Users");
 var number = Math.floor(Math.random()*10);
@@ -15,8 +16,26 @@ User.getUsers = function(callback){
     
     User.find({}).exec(function(err,result){
         if(err) callback(err,null);
-        console.log(number);
-        callback(null,result[number]);
+        
+        if(result.length == 0){
+            console.log("NO USERS");
+            request.get("http://www.filltext.com/?rows=10&firstName={firstName}&lastName={lastName}&pretty=true",function(error,response,body){
+                var users = JSON.parse(body);
+                User.addingUsers(users,function(err,resultres){
+                    if(err) console.error(err);
+                    
+                    User.find({}).exec(function(err,result){
+                        if(err) callback(err,null);
+                        callback(null,result[number]);
+                    })
+                });
+            });
+        }
+
+        else{
+            console.log("USERS IN DB");
+            callback(null,result[number]);
+        }
     });
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var request = require("request");
-var User = require("../data/models/user");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  request.get("http://www.filltext.com/?rows=10&firstName={firstName}&lastName={lastName}&pretty=true",
-  function (error, response, body) {
-      var users = JSON.parse(body);
-      User.addingUsers(users,function(err,resultres){
-        if(err) console.error(err);
-        res.render("index");
-      });
-  });
-
-
-
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
